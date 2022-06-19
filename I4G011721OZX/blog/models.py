e1e83c16721c55d4f935fc022a39b8327c063ec8
harkeju <61701308+harkeju@users.noreply.github.com>
2022-06-19 13:34:49 +0100

    First commit

---
+++ I4G011721OZX/blog/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    author = models.ForeignKey(get_user_mode)
